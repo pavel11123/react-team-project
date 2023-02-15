bf5f30449561ec0652fc86a7c2725c579df6d09b
Rastyapaa <Limonchik300@gmail.com>
2023-02-15 15:51:14 +0500

    Добавил header

--- src/index.jsx
+++ src/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./components/App/App";
-
+import AppHeader from "./AppHeader/AppHeader";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <AppHeader />
     <App />
+    
   </React.StrictMode>
 );
